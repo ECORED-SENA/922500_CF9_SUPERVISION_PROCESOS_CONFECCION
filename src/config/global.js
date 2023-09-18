2fb671e632cae144a8e250d6ea00f781cf097417
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-09-18 13:52:13 -0500

    zip Accesibilidad

--- src/config/global.js
+++ src/config/global.js
@@ -162,7 +162,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/922500-CF09-DU.zip',
       },
       {
         icono: 'fas fa-download',
